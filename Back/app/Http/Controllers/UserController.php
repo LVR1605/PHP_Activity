86600a4c3b47c4185d657dd1bd9838384379a91f
lvr1605 <dolphinlvr1605@gmail.com>
2023-06-15 13:33:53 +0800

    3rd

--- Back/app/Http/Controllers/UserController.php
+++ Back/app/Http/Controllers/UserController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\Cast\String_;
 
 class UserController extends Controller
 {
     //
+    public function index(){
+
+        $data = User::paginate(20);
+              
+        return $data;
+    }
+    public function getID($id){
+
+        $data = User::find($id);
+              
+        return $data;
+    }
 }
--- Back/routes/api.php
+++ Back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\UserController;
@@ -18,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/users', function (Request $request) {
     return $request->users();
 });
+
+Route::get('/user', [UserController::class, 'index']) -> name('login');
+Route::get('/user/{id}', [UserController::class, 'getID']) -> name('login');
